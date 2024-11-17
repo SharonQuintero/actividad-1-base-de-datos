ab788c605b1b4057dcac08eba0f93ade7fb154fd
SharonQuintero <69642371+SharonQuintero@users.noreply.github.com>
2024-11-17 11:39:39 -0500

    Add files via upload

---
+++ scripts_act1.sh
@@ -0,0 +1,10 @@
+#! Crear base de datos
+use Pruebas
+
+#! Crear colecciones
+db.createCollection("Deportistas")
+db.createCollection("Entrenadores")
+db.createCollection("Equipos")
+db.createCollection("Arbitros")
+db.createCollection("Encuentros")
+db.createCollection("TablaPosiciones")
